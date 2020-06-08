a833d7577030e6b4a6fbe1ac75ccbc420bb8c93e
Owolabi Oromidayo <owolabi.oromidayo@stu.cu.edu.ng>
2020-06-08 13:55:15 +0100

    Add puzzle generation

---
+++ input_window.py
@@ -0,0 +1,43 @@
+import tkinter as tk
+class Choice:
+    """Make choice on cell"""
+    #remove choice, make choice
+
+    def __init__(self, parent, cell_id):
+        self.parent = parent  # this is an instance of the sudoku class
+        self.cell_id = cell_id
+        self.build()
+
+
+    def build(self):
+        self.root = tk.Tk()
+        self.root.title('Cell Options')
+        self.root.bind('<Return>', self.handle_event)
+        self.label = tk.Label(self.root, text="Make a Choice")
+        self.label.grid(row=0, column=0, pady=10, padx=10)
+
+        self.choice = tk.IntVar(self.root)
+
+        self.rbtn1 = tk.Radiobutton(self.root, text="Remove Choice (if any)", variable=self.choice, value=0)
+        self.rbtn1.grid(row=1, column=0)
+
+        self.rbtn2 = tk.Radiobutton(self.root, text="Make Choice", variable=self.choice, value=1)
+        self.rbtn2.grid(row=2, column=0)
+
+        self.button = tk.Button(self.root, text='Confirm', command=self.handle_choice)
+        self.button.grid(row=3, column=0, pady=10)
+
+        self.root.mainloop()
+
+    def handle_choice(self):
+        self.choice = self.choice.get()
+        self.root.destroy()
+
+        if self.choice == 0: #remove choice
+           self.parent.board.delete(self.cell_id)
+
+        else:
+           
+
+    def handle_event(self, event):
+        self.handle_choice()
--- main.py
+++ main.py
@@ -1,19 +1,35 @@
 import tkinter as tk
+import random
+import copy
+from choice_window import Choice
 
 class Sudoku:
-    def __init__(self):
-        self.width, self.height = 800,800
-        self.build()
+    def __init__(self, mode, difficulty):
+        if mode == 'debug':
+            self.gen_puzzle()
+            
+
+        else:
+
+            self.width, self.height = 800,800
+            self.difficulty = difficulty
+            self.game = [[[None for i in range(3)] for i in range(3)] for i in range(9)]
+            self.cellsize =  self.width // 9
+            self.build()
+
 
 
     def build(self):
         self.root = tk.Tk()
         self.root.geometry(f"{self.width}x{self.height}")
         self.root.resizable(width=False, height=False)
+        self.root.bind("<Button-1>", self.handleclick)  
 
         self.board = tk.Canvas(self.root,width=self.width, height=self.height)
         self.board.pack()
+
         self.gen_lines(7,3)
+        self.make_puzzle()
 
         self.root.mainloop()
 
@@ -30,28 +46,7 @@ class Sudoku:
     def gen_lines(self, b_width,s_width):
 
         self.gen_lines_recursive(0,0, self.width,  b_width)
-        # step = self.width // 3
-        # pos = [step*i for i in range(1,3)]
 
-        # #main grid lines
-        # for i in range(2):
-        #     self.board.create_line(0,pos[i], self.width,pos[i], width=b_width) #vertical lines
-        #     self.board.create_line(pos[i],0,pos[i],self.height, width= b_width) #horizontal lines
-
-
-        # step = step // 3
-        # pos = [step*i for i in range(1,3]
-        # #inner grid lines
-        # for i in range(9):
-        #     for j in range(3):
-        #         #verts
-        #         self.board.create_line(pos)
-
-
-        # step2 = step//3
-        # pos2 = [step2*i for i in range(1,3)]
-
-        
         tlc = {} #idx to pos
         for y in range(3):
             for x in range(3):
@@ -64,20 +59,112 @@ class Sudoku:
             y,x = [idx*small_step for idx in tlc[i]]
             self.gen_lines_recursive(x, y, small_step, s_width)
 
+    def pos_to_loc(self, x, y):
+        loc_x = x// self.cellsize
+        loc_y = y// self.cellsize
+        return loc_x, loc_y
 
-        # for i in range(9):
-        #     x,y = [i*step2 for i in tlc[i]]
-            
-            
-        #     for i in range(2):
-        #         self.board.create_line(x,y, step+x,y, width=s_width)
-        #         self.board.create_line(x,y, x,step+y, width=s_width)
+    def loc_to_pos(self, x, y):
+        pos_x, pos_y = x*self.cellsize, y*self.cellsize
+        #center positions
+        pos_x += self.cellsize // 2
+        pos_y += self.cellsize // 2
 
-                # self.board.create_line(pos2[i],0,pos2[i],step, width= b_width)
+        return pos_x, pos_y
 
-            
+    def handleclick(self, event):
+        x,y = self.pos_to_loc(event.x, event.y)
+        cell_id = y*9+x
+        print(cell_id)
+        Choice(self, str(cell_id) )
+
+
+        
+
+    def flatten(self,arr):
+        return [j for j in i for i in arr]
+
+    def reshape(self, arr, step=3):
+        #converts a 1d array to a 2d array, each row of size step, provided step*2 values
+        ls = []
+        for i in range(step):
+            ls.append([j for j in arr[step*i:step*i+step] ] )
+
+        return ls
+
+
+    def shift_ls(self, ls, shift):
+
+        #shifts flattened array
+        for _ in range(shift):
+           left = ls.pop(0)
+           ls.append(left)
+
+        return ls
+
+
+    def gen_puzzle(self):
+        seed_nums = [i for i in range(1,10)]
+        random.shuffle(seed_nums)
+
+        puzzle = []
+        shift_vals  = [0,3,3,1,3,3,1,3,3]
+        for idx in shift_vals:
+           
+            seed_nums = self.shift_ls(seed_nums, idx)
+            row = copy.copy(seed_nums)
+            # row = self.reshape(row)
+            puzzle.append(row)
+
+        
+        #print puzzle
+        # for row in puzzle:
+        #     for val in row:
+        #         print(str(val) +' ', end='')
+        #     print('')
+
+        return puzzle
+
+
+    def make_puzzle(self):
+        diffs = {'easy':0.5, 'medium': 0.6, 'hard': 0.8}
+
+        removal_rate = diffs[self.difficulty]
+        num_to_remove = int(removal_rate * 81)
+        puzzle = self.gen_puzzle()
+
+        #apply removal rate to puzzle
+        for _ in range(num_to_remove):
+            _range = [i for i in range(9)]
+            x,y = random.choice(_range), random.choice(_range)
+            puzzle[y][x] = None
+
+
+        #print puzzle on board
+        for row_idx in range(len(puzzle)):
+            for col_idx in range(len(puzzle)):
+                x,y = col_idx, row_idx
+                pos_x, pos_y = self.loc_to_pos(x,y)
+                cell_id = y*9+x
+
+                self.draw(pos_x, pos_y, puzzle[row_idx][col_idx], cell_id )
+                
+
+
+    def draw(self, x, y, text, cell_id):
+        if text is not None:
+            self.board.create_text(x, y, text=text, font=('Times', 20), tag= str(cell_id) )
+
+        else:
+            self.board.create_text(x, y, text='', font=('Times', 20), tag= str(cell_id) )
+
+
+
+    
+
+
 
 
 
 if __name__ == "__main__":
-    Sudoku()
+    s = Sudoku(mode = 'main', difficulty='hard' )
